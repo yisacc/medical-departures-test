5e5fcd4ce0e2da2e79d390bb9fd7b3682f9e7aec
Yisacc Aberham <isaccab2019@gmail.com>
2023-02-27 00:05:32 +0300

    added update query

--- src/controllers/blogs.ts
+++ src/controllers/blogs.ts
@@ -10,18 +10,15 @@ import HttpResponse from '../utils/httpResponse';
 
 
 blogsRouter.get('/', asyncHandler(async (_req, res) => {
-  const query= 'SELECT "id", "title", "author", "url" FROM blogs'
-  const blogs = await sequelize.query(query,{
-    model:Blog,
-    mapToModel:true
-  })
-  const httpResponse = HttpResponse.get(blogs)
+  const query= 'SELECT id, title, author, url FROM blogs'
+  const [results] = await sequelize.query(query)
+  const httpResponse = HttpResponse.get(results)
   res.status(200).json(httpResponse)
 }))
 
 blogsRouter.post('/', asyncHandler(async (req, res) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const {title,author,url,user_id}=req.body;
+  const {title,author,url,user_id}:{title:string,author:string,url:string,user_id:string}=req.body;
   const query=`
   INSERT INTO blogs
   (
@@ -32,15 +29,18 @@ blogsRouter.post('/', asyncHandler(async (req, res) => {
     '${title}', '${author}', '${url}','${Number(user_id)}'
   )
 `;
-    const blog = await sequelize.query(query)
-    const httpResponse = HttpResponse.created(blog)
+const [results] = await sequelize.query(query)
+    const httpResponse = HttpResponse.created(results)
     res.status(201).json(httpResponse)
 }))
 
 blogsRouter.get('/:id', asyncHandler(async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    res.json(blog)
+  const id=Number(req.params.id)
+  const query=`SELECT id, title, author, url FROM blogs WHERE id = ${id}`
+  const [results]=await sequelize.query(query)
+  if (results.length) {
+    const httpResponse = HttpResponse.get(results)
+    res.status(201).json(httpResponse)
   } else {
     res.status(404).end()
   }
@@ -54,15 +54,24 @@ blogsRouter.delete('/:id', asyncHandler(async (req, res) => {
   res.status(204).end()
 }))
 
-// blogsRouter.put('/:id', asyncHandler(async (req, res) => {
-//   const blog = await Blog.findByPk(req.params.id)
-//   if (blog) {
-//     blog.title = req.body.title
-//     await blog.save()
-//     res.json(blog)
-//   } else {
-//     res.status(404).end()
-//   }
-// }))
+blogsRouter.put('/:id', asyncHandler(async (req, res) => {
+  const id=Number(req.params.id)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const {title,author,url}:
+  {title:string,author:string,url:string}=req.body;
+  const query=`UPDATE blogs
+   SET title='${title}',
+    author='${author}',
+     url='${url}' 
+     WHERE id = ${id}
+     returning *`
+  const [results]=await sequelize.query(query)
+  if (results.length) {
+    const httpResponse = HttpResponse.updated(results)
+    res.status(200).json(httpResponse)
+  } else {
+    res.status(404).end()
+  }
+}))
 
 export default blogsRouter
--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -3,20 +3,15 @@ const usersRouter = express.Router();
 import asyncHandler from 'express-async-handler';
 import bcrypt from 'bcrypt';
 import {sequelizeConnection as sequelize} from '../utils/db';
-
-import {User} from '../models'
 import HttpResponse from '../utils/httpResponse';
 
 
 
 
 usersRouter.get('/', asyncHandler(async (_req, res) => {
-  const query= 'SELECT * FROM users'
-  const blogs = await sequelize.query(query,{
-    model:User,
-    mapToModel:true
-  })
-  const httpResponse = HttpResponse.get(blogs)
+  const query= 'SELECT id,username,name FROM users'
+  const [results] = await sequelize.query(query)
+  const httpResponse = HttpResponse.get(results)
   res.status(200).json(httpResponse)
 }))
 
@@ -34,10 +29,42 @@ usersRouter.post('/', asyncHandler(async (req, res) => {
   (
     '${username}', '${name}', '${passwordHash}'
   )
+  RETURNING id
 `;
-    const blog = await sequelize.query(query)
-    const httpResponse = HttpResponse.created(blog)
+    const [results] = await sequelize.query(query)
+    const httpResponse = HttpResponse.created(results)
     res.status(201).json(httpResponse)
+}))
+
+usersRouter.get('/:id', asyncHandler(async (req, res) => {
+  const id=Number(req.params.id)
+  const query=`SELECT username, name FROM users WHERE id = ${id}`
+  const [results]=await sequelize.query(query)
+  if (results.length) {
+    const httpResponse = HttpResponse.get(results)
+    res.status(201).json(httpResponse)
+  } else {
+    res.status(404).end()
+  }
+}))
+
+usersRouter.put('/:id', asyncHandler(async (req, res) => {
+  const id=Number(req.params.id)
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const {username,name}:
+  {username:string,name:string}=req.body;
+  const query=`UPDATE users
+   SET username='${username}',
+    name='${name}'
+     WHERE id = ${id}
+     returning id,username,name`
+  const [results]=await sequelize.query(query)
+  if (results.length) {
+    const httpResponse = HttpResponse.updated(results)
+    res.status(200).json(httpResponse)
+  } else {
+    res.status(404).end()
+  }
 }))
 
 export default usersRouter
