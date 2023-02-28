1637f9c50725f26bef947e4805a2171bfc89d595
Yisacc Aberham <isaccab2019@gmail.com>
2023-03-01 01:00:29 +0300

    updated swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
 import express from 'express';
 const app = express();
 import cors from 'cors';
-import yaml from 'js-yaml'
-import fs from 'fs'
-import path from 'path'
 import swaggerUi from 'swagger-ui-express'
 import * as middleware from './utils/middleware';
 import * as logger from './utils/logger';
 import {connectToDatabase} from './utils/db';
-import SwaggerInit from './swagger/init'
 import routes from './routes'
+import { swaggerDocument } from './swagger';
 
 async function main() {
 await connectToDatabase()
@@ -18,15 +15,8 @@ await connectToDatabase()
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
-
 	// set swagger
-	try {
-		await SwaggerInit();
-		const oas3Specification: any = yaml.load(fs.readFileSync(path.resolve(__dirname,'./swagger/backend_api.yaml'), 'utf8'))
-		app.use('/swagger', swaggerUi.serve, swaggerUi.setup(oas3Specification, {explorer:true}))
-	} catch (error) {
-		logger.error(`main(): Swagger error: ${error}`)
-	}
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {explorer:true}))
 app.get('/ping', (_req, res) => {
     logger.info('someone pinged here');
     res.send('pong');
--- src/swagger/index.ts
+++ src/swagger/index.ts
@@ -1,51 +1,19 @@
-export default {
-  definition: {
-    openapi: '3.0.2',
-    info: {
-      version: 'v0',
+export const swaggerDocument = {
+  openapi: '3.0.2',
+  info: {
+      version: '1.0.0',
       title: 'Medical Departures Test API',
       description: 'Website documentation to create a CRUD blog API with authentication in NodeJs using TypeScript',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-        description: 'LOCAL 3000',
-      },
-    ],
-    components: {
-      schemas: {
-        User: {
-          type: 'object',
-          properties: {
-            username: {
-              type: 'string',
-            },
-            password: {
-              type: 'string',
-            },
-            first_name: {
-              type: 'string',
-            },
-            last_name: {
-              type: 'string',
-            },
-            id_role: {
-              type: 'number',
-            },
-            is_active: {
-              type: 'boolean',
-            },
-          },
-        },
-      },
-      securitySchemes: {
-        BasicAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
-    },
+      termsOfService: '',
+      contact: {
+          name: 'Yisacc Aberham',
+          email: 'isaccab2019@gmail.com',
+      }
   },
-  apis: ['./routes/**/*.ts'],
+  servers:[
+    {
+      url:'http://localhost:3000/api',
+      description:'Local Server'
+    }
+  ]
 }
