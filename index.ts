760de326dac78c2b3561037ee3a93af04564abcc
Yisacc Aberham <isaccab2019@gmail.com>
2023-02-28 22:58:02 +0300

    move back the index file to the src folder

--- index.ts
+++
@@ -1,8 +0,0 @@
-import app from './src/app';
-import {PORT} from './src/utils/config';
-import * as logger from './src/utils/logger';
-
-
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
-})
